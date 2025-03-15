8b4a10d9b2037d06fbeb71bd6b5c97ad56b9a432
Vicky <vicky2003211@example.com>
2025-03-15 20:25:02 +0530

    Cors issue

--- ceg_affidavits/app.js
+++ ceg_affidavits/app.js
@@ -9,8 +9,6 @@ const connectDB = require("./db/config"); // Import your DB connection
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-const cors = require("cors");
 
 const allowedOrigins = [
   "http://localhost:3000",  // Allow local development
