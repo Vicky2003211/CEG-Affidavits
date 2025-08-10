9a8aa52ad72701068b43afc26df73e267339d861
Vicky <vicky2003211@example.com>
2025-08-10 19:05:34 +0530

    Updated Header

--- ceg_front/src/index.js
+++ ceg_front/src/index.js
@@ -4,7 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import favicon from "./Assets/image.png";
 
+// ✅ Set global browser tab title
+document.title = "CEG-Affidavits";
+
+// ✅ Set global favicon (logo)
+const link = document.querySelector("link[rel~='icon']");
+if (link) {
+  link.href = favicon; // Path from public folder
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,7 +22,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
